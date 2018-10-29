71329187da05a1d392945fd3aa34dee3baf8b6e8
autodocs <autodocs>
2018-10-29 15:24:06 +0000

    build based on cb40994

--- dev/search_index.js
+++ dev/search_index.js
@@ -85,7 +85,7 @@ var documenterSearchIndex = {"docs": [
     "page": "API",
     "title": "FinancialDerivatives.AmericanOption",
     "category": "type",
-    "text": "AmericanOption(s, k, r, σ, t, call)\n\nArguments\n\ns: underlying price\nk: strike price\nr: risk-free interest rate\nσ: volatility\nt: time to expiration\ncall: 1 if call, -1 if put\n\n\n\n\n\n"
+    "text": "AmericanOption(s, k, r, σ, t, call)\n\nAmerican option.\n\nArguments\n\ns: underlying price\nk: strike price\nr: risk-free interest rate\nσ: volatility\nt: time to expiration\ncall: 1 if call, -1 if put\n\n\n\n\n\n"
 },
 
 {
@@ -93,7 +93,7 @@ var documenterSearchIndex = {"docs": [
     "page": "API",
     "title": "FinancialDerivatives.AsianOption",
     "category": "type",
-    "text": "AsianOption(s, k, r, σ, t, call)\n\nArguments\n\ns: underlying price\nk: strike price\nr: risk-free interest rate\nσ: volatility\nt: time to expiration\ncall: 1 if call, -1 if put\n\n\n\n\n\n"
+    "text": "AsianOption(s, k, r, σ, t, call)\n\nAsian option.\n\nArguments\n\ns: underlying price\nk: strike price\nr: risk-free interest rate\nσ: volatility\nt: time to expiration\ncall: 1 if call, -1 if put\n\n\n\n\n\n"
 },
 
 {
@@ -101,7 +101,7 @@ var documenterSearchIndex = {"docs": [
     "page": "API",
     "title": "FinancialDerivatives.EuropeanOption",
     "category": "type",
-    "text": "EuropeanOption(s, k, r, σ, t, call)\n\nArguments\n\ns: underlying price\nk: strike price\nr: risk-free interest rate\nσ: volatility\nt: time to expiration\ncall: 1 if call, -1 if put\n\n\n\n\n\n"
+    "text": "EuropeanOption(s, k, r, σ, t, call)\n\nEuropean option.\n\nArguments\n\ns: underlying price\nk: strike price\nr: risk-free interest rate\nσ: volatility\nt: time to expiration\ncall: 1 if call, -1 if put\n\n\n\n\n\n"
 },
 
 {
@@ -109,7 +109,7 @@ var documenterSearchIndex = {"docs": [
     "page": "API",
     "title": "FinancialDerivatives.FXOption",
     "category": "type",
-    "text": "FXOption(s, k, r_d, r_f, σ, t, call)\n\nArguments\n\ns: underlying price\nk: strike price\nr_d: domestic risk-free interest rate\nr_f: foreign risk-free interest rate\nσ: volatility\nt: time to expiration\ncall: 1 if call, -1 if put\n\n\n\n\n\n"
+    "text": "FXOption(s, k, r_d, r_f, σ, t, call)\n\nForeign exchange option.\n\nArguments\n\ns: underlying price\nk: strike price\nr_d: domestic risk-free interest rate\nr_f: foreign risk-free interest rate\nσ: volatility\nt: time to expiration\ncall: 1 if call, -1 if put\n\n\n\n\n\n"
 },
 
 {
@@ -117,7 +117,7 @@ var documenterSearchIndex = {"docs": [
     "page": "API",
     "title": "FinancialDerivatives.InterestRateDerivative",
     "category": "type",
-    "text": "InterestRateDerivative(k, r, σ, θ, t)\n\nArguments\n\nk: speed of reversion\nr: initial interest rate\nσ: instantaneous volatility\nθ: long term mean level\nt: time interval\n\n\n\n\n\n"
+    "text": "InterestRateDerivative(k, r, σ, θ, t)\n\nInterest rate derivative.\n\nArguments\n\nk: speed of reversion\nr: initial interest rate\nσ: instantaneous volatility\nθ: long term mean level\nt: time interval\n\n\n\n\n\n"
 },
 
 {
@@ -133,7 +133,7 @@ var documenterSearchIndex = {"docs": [
     "page": "API",
     "title": "FinancialDerivatives.BlackKarasinski",
     "category": "type",
-    "text": "BlackKarasinski model\n\n\n\n\n\n"
+    "text": "BlackKarasinski()\n\nBlack-Karasinski model.\n\n\n\n\n\n"
 },
 
 {
@@ -141,7 +141,7 @@ var documenterSearchIndex = {"docs": [
     "page": "API",
     "title": "FinancialDerivatives.BlackScholes",
     "category": "type",
-    "text": "Black-Scholes model\n\n\n\n\n\n"
+    "text": "BlackScholes()\n\nBlack-Scholes model.\n\n\n\n\n\n"
 },
 
 {
@@ -149,7 +149,7 @@ var documenterSearchIndex = {"docs": [
     "page": "API",
     "title": "FinancialDerivatives.BrennanSchwartz",
     "category": "type",
-    "text": "BrennanSchwartz model\n\n\n\n\n\n"
+    "text": "BrennanSchwartz()\n\nBrennan and Schwartz model.\n\n\n\n\n\n"
 },
 
 {
@@ -157,7 +157,7 @@ var documenterSearchIndex = {"docs": [
     "page": "API",
     "title": "FinancialDerivatives.CoxIngersollRoss",
     "category": "type",
-    "text": "CoxIngersollRoss model\n\n\n\n\n\n"
+    "text": "CoxIngersollRoss()\n\nCox-Ingersoll-Ross model.\n\n\n\n\n\n"
 },
 
 {
@@ -165,7 +165,7 @@ var documenterSearchIndex = {"docs": [
     "page": "API",
     "title": "FinancialDerivatives.CoxRossRubinstein",
     "category": "type",
-    "text": "CoxRossRubinstein binomial model\n\n\n\n\n\n"
+    "text": "CoxRossRubinstein()\n\nCox-Ross-Rubinstein binomial model (aka Binomial options pricing model).\n\n\n\n\n\n"
 },
 
 {
@@ -173,7 +173,7 @@ var documenterSearchIndex = {"docs": [
     "page": "API",
     "title": "FinancialDerivatives.GarmanKohlhagen",
     "category": "type",
-    "text": "Garman–Kohlhagen model\n\n\n\n\n\n"
+    "text": "GarmanKohlhagen()\n\nGarman–Kohlhagen model.\n\n\n\n\n\n"
 },
 
 {
@@ -181,7 +181,7 @@ var documenterSearchIndex = {"docs": [
     "page": "API",
     "title": "FinancialDerivatives.JarrowRudd",
     "category": "type",
-    "text": "JarrowRudd model\n\n\n\n\n\n"
+    "text": "JarrowRudd()\n\nJarrow-Rudd model.\n\n\n\n\n\n"
 },
 
 {
@@ -189,7 +189,7 @@ var documenterSearchIndex = {"docs": [
     "page": "API",
     "title": "FinancialDerivatives.LeisenReimer",
     "category": "type",
-    "text": "LeisenReimer binomial model\n\n\n\n\n\n"
+    "text": "LeisenReimer()\n\nLeisen-Reimer binomial model.\n\n\n\n\n\n"
 },
 
 {
@@ -197,7 +197,7 @@ var documenterSearchIndex = {"docs": [
     "page": "API",
     "title": "FinancialDerivatives.LongstaffSchwartz",
     "category": "type",
-    "text": "LongstaffSchwartz model\n\n\n\n\n\n"
+    "text": "LongstaffSchwartz()\n\nLongstaff-Schwartz model.\n\n\n\n\n\n"
 },
 
 {
@@ -205,7 +205,7 @@ var documenterSearchIndex = {"docs": [
     "page": "API",
     "title": "FinancialDerivatives.RendlemanBartter",
     "category": "type",
-    "text": "RendlemanBartter model\n\n\n\n\n\n"
+    "text": "RendlemanBartter()\n\nRendleman-Bartter model.\n\n\n\n\n\n"
 },
 
 {
@@ -213,7 +213,7 @@ var documenterSearchIndex = {"docs": [
     "page": "API",
     "title": "FinancialDerivatives.Tian",
     "category": "type",
-    "text": "Tian binomial model\n\n\n\n\n\n"
+    "text": "Tian()\n\nTian binomial model.\n\n\n\n\n\n"
 },
 
 {
@@ -221,7 +221,7 @@ var documenterSearchIndex = {"docs": [
     "page": "API",
     "title": "FinancialDerivatives.Vasicek",
     "category": "type",
-    "text": "Vasicek model\n\n\n\n\n\n"
+    "text": "Vasicek()\n\nVasicek model.\n\n\n\n\n\n"
 },
 
 {
@@ -237,7 +237,7 @@ var documenterSearchIndex = {"docs": [
     "page": "API",
     "title": "FinancialDerivatives.evaluate",
     "category": "function",
-    "text": "evaluate(IRD, BlackKarasinski(), n = 12)\n\nEvaluate interest rate derivative using BlackKarasinski model.\n\nArguments\n\nIRD::InterestRateDerivative: InterestRateDerivative\nn: number of paths to simulate\n\n\n\n\n\nevaluate(O, BlackScholes())\n\nEvaluate option O using Black-Scholes model.\n\nArguments\n\nO::Option: Option\n\n\n\n\n\nevaluate(o)\n\nEvaluate option o using Back-Scholes model as default valuation model.\n\n\n\n\n\nevaluate(IRD, BrennanSchwartz(), n = 12)\n\nEvaluate interest rate derivative IRD using BrennanSchwartz model.\n\nArguments\n\nIRD::InterestRateDerivative: interest rate derivative\nn: number of paths to simulate\n\n\n\n\n\nevaluate(IRD, CoxIngersollRoss(), n)\n\nEvaluate interest rate derivative IRD using CoxIngersollRoss model.\n\nArguments\n\nIRD::InterestRateDerivative: interest rate derivative\nn: number of paths to simulate\n\n\n\n\n\nevaluate(O, CoxRossRubinstein(), N = 1000)\n\nEvaluate option O using CoxRossRubinstein.\n\nArguments\n\nO::Option: option\nN: number of paths to simulate\n\n\n\n\n\nevaluate(O::FXOption, GarmanKohlhagen())\n\nEvaluate FX Option using Garman–Kohlhagen model.\n\nArguments\n\nO::FXOption\n\n\n\n\n\nevaluate(O, JarrowRudd(), risk_neutral = true, N = 1000)\n\nEvaluate option O using JarrowRudd binomial model (defaults to the risk-neutral version).\n\nArguments\n\nO::Option: option\nrisk_neutral: true if risk neutral, false if equal probability.\nN: number of paths to simulate\n\n\n\n\n\nevaluate(O, LeisenReimer(), N = 1001)\n\nEvaluate option O using LeisenReimer binomial model.\n\nArguments\n\nN: number of paths to simulate, must be odd\n\n\n\n\n\nevaluate(O, LongstaffSchwartz(), N = 1000, P = 10000)\n\nEvaluate option O using LongstaffSchwartz binomial model.\n\nArguments\n\nN: number of paths to simulate\nP: number of periods\n\n\n\n\n\nevaluate(IRD, RendlemanBartter(), n = 12)\n\nEvaluate interest rate derivative IRD using RendlemanBartter model.\n\nArguments\n\nn: number of paths to simulate\n\n\n\n\n\nevaluate(O, RendlemanBartter(), k = 1, N = 1000)\n\nEvaluate option O using RendlemanBartter model.\n\nArguments\n\nk:\nN: \n\n\n\n\n\nevaluate(O, Tian(), N = 1000)\n\nEvaluate option O using Tian binomial model.\n\nArguments\n\nN: number of paths to simulate\n\n\n\n\n\nevaluate(IRD, Vasicek(), n = 12)\n\nEvaluate interest rate derivative IRD using  Vasicek model.\n\nArguments\n\nn: number of paths to simulate\n\n\n\n\n\n"
+    "text": "evaluate(IRD, BlackKarasinski(), n = 12)\n\nEvaluate interest rate derivative using BlackKarasinski model.\n\nArguments\n\nIRD::InterestRateDerivative: interest rate derivative\nn: number of paths to simulate\n\n\n\n\n\nevaluate(O, BlackScholes())\n\nEvaluate option O using BlackScholes model.\n\nArguments\n\nO::Option: Option\n\n\n\n\n\nevaluate(o)\n\nEvaluate option o using Back-Scholes model as default valuation model.\n\n\n\n\n\nevaluate(IRD, BrennanSchwartz(), n = 12)\n\nEvaluate interest rate derivative IRD using BrennanSchwartz model.\n\nArguments\n\nIRD::InterestRateDerivative: interest rate derivative\nn: number of paths to simulate\n\n\n\n\n\nevaluate(IRD, CoxIngersollRoss(), n)\n\nEvaluate interest rate derivative IRD using CoxIngersollRoss model.\n\nArguments\n\nIRD::InterestRateDerivative: interest rate derivative\nn: number of paths to simulate\n\n\n\n\n\nevaluate(O, CoxRossRubinstein(), N = 1000)\n\nEvaluate option O using CoxRossRubinstein.\n\nArguments\n\nO::Option: option\nN: number of paths to simulate\n\n\n\n\n\nevaluate(O::FXOption, GarmanKohlhagen())\n\nEvaluate FX Option using GarmanKohlhagen model.\n\nArguments\n\nO::FXOption\n\n\n\n\n\nevaluate(O, JarrowRudd(), risk_neutral = true, N = 1000)\n\nEvaluate option O using JarrowRudd binomial model (defaults to the risk-neutral version).\n\nArguments\n\nO::Option: option\nrisk_neutral: true if risk neutral, false if equal probability.\nN: number of paths to simulate\n\n\n\n\n\nevaluate(O, LeisenReimer(), N = 1001)\n\nEvaluate option O using LeisenReimer binomial model.\n\nArguments\n\nN: number of paths to simulate, must be odd\n\n\n\n\n\nevaluate(O, LongstaffSchwartz(), N = 1000, P = 10000)\n\nEvaluate option O using LongstaffSchwartz binomial model.\n\nArguments\n\nN: number of paths to simulate\nP: number of periods\n\n\n\n\n\nevaluate(IRD, RendlemanBartter(), n = 12)\n\nEvaluate interest rate derivative IRD using RendlemanBartter model.\n\nArguments\n\nn: number of paths to simulate\n\n\n\n\n\nevaluate(O, RendlemanBartter(), k = 1, N = 1000)\n\nEvaluate option O using RendlemanBartter model.\n\nArguments\n\nk:\nN: \n\n\n\n\n\nevaluate(O, Tian(), N = 1000)\n\nEvaluate option O using Tian binomial model.\n\nArguments\n\nN: number of paths to simulate\n\n\n\n\n\nevaluate(IRD, Vasicek(), n = 12)\n\nEvaluate interest rate derivative IRD using Vasicek model.\n\nArguments\n\nn: number of paths to simulate\n\n\n\n\n\n"
 },
 
 {
@@ -253,7 +253,7 @@ var documenterSearchIndex = {"docs": [
     "page": "API",
     "title": "FinancialDerivatives.Derivative",
     "category": "type",
-    "text": "Abstract type for Derivative\n\n\n\n\n\n"
+    "text": "Abstract type for derivative contract.\n\n\n\n\n\n"
 },
 
 {
@@ -261,7 +261,7 @@ var documenterSearchIndex = {"docs": [
     "page": "API",
     "title": "FinancialDerivatives.Forward",
     "category": "type",
-    "text": "Abstract type for Forward\n\n\n\n\n\n"
+    "text": "Abstract type for forward contract.\n\n\n\n\n\n"
 },
 
 {
@@ -269,7 +269,7 @@ var documenterSearchIndex = {"docs": [
     "page": "API",
     "title": "FinancialDerivatives.Future",
     "category": "type",
-    "text": "Abstract type for Future\n\n\n\n\n\n"
+    "text": "Abstract type for futures contract.\n\n\n\n\n\n"
 },
 
 {
@@ -277,7 +277,7 @@ var documenterSearchIndex = {"docs": [
     "page": "API",
     "title": "FinancialDerivatives.Option",
     "category": "type",
-    "text": "Abstract type for Option\n\n\n\n\n\n"
+    "text": "Abstract type for option.\n\n\n\n\n\n"
 },
 
 {
@@ -285,7 +285,7 @@ var documenterSearchIndex = {"docs": [
     "page": "API",
     "title": "FinancialDerivatives.Swap",
     "category": "type",
-    "text": "Abstract type for Swap\n\n\n\n\n\n"
+    "text": "Abstract type for swap.\n\n\n\n\n\n"
 },
 
 {
@@ -293,7 +293,7 @@ var documenterSearchIndex = {"docs": [
     "page": "API",
     "title": "FinancialDerivatives.Swaption",
     "category": "type",
-    "text": "Abstract type for Swaption\n\n\n\n\n\n"
+    "text": "Abstract type for swaption.\n\n\n\n\n\n"
 },
 
 {
