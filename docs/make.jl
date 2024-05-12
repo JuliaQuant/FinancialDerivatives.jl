3a7df848f6f49ec86c23bff9e6873651d5614a6d
mauricio.vanzulli <mcvanzulli@gmail.com>
2024-05-12 11:58:19 +0200

    push pages preview on CI

--- docs/make.jl
+++ docs/make.jl
@@ -3,8 +3,9 @@ using FinancialDerivatives
 
 makedocs(; format=:html,
          sitename="FinancialDerivatives.jl",
+         clean=true, doctest=false, linkcheck=true,
          pages=["index.md",
                 "getting_started.md",
                 "api.md"])
 
-deploydocs(; repo="github.com/JuliaQuant/FinancialDerivatives.jl.git")
+deploydocs(; repo="github.com/JuliaQuant/FinancialDerivatives.jl.git", push_preview=true)
